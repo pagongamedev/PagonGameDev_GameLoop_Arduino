08995f1c6932033d511813be8a493871f9d2a4a1
Pagon Game Dev <pagongamedev@gmail.com>
2016-04-09 05:15:08 +0700

    Add PagonGameDev_GameLoop Library

---
+++ PagonGameDev_GameLoop.cpp
@@ -0,0 +1,104 @@
+//================================================
+// Dev by : Pagon Game Dev
+// PagonGameDev@gmail.com
+// pagongamedev.com
+//================================================
+
+#include "Arduino.h"
+#include "PagonGameDev_GameLoop.h"
+
+GameInterval::GameInterval(int iInputInterval, void (funcInterval)())
+{
+  iMillisInterval = iInputInterval;
+  functionInterval = funcInterval;
+
+  iMillisIntervalDelayTime = iInputInterval;
+}
+
+void GameInterval::update()
+{
+  lMillisCurrent = millis();
+
+  if (lMillisCurrent - lMillisPrevious >= iMillisIntervalDelayTime) {
+
+    //Skip Delay Time
+    iMillisDelayTime = ((lMillisCurrent - lMillisPrevious) - (iMillisInterval - iMillisDelayTime )) % iMillisInterval;
+    iMillisIntervalDelayTime = iMillisInterval - iMillisDelayTime;
+
+    DeltaTime = (lMillisCurrent - lMillisPrevious) / 1000.0d;
+
+    lMillisPrevious = lMillisCurrent;
+    functionInterval();
+
+  }
+}
+
+//================================================================
+GameInput::GameInput()
+{
+}
+
+bool GameInput::getKey(int iPinInput, bool isTrueState)
+{
+  bool isRe = false;
+
+  if (digitalRead(iPinInput) == isTrueState)
+  {
+    isRe = true;
+  } else {
+    isRe = false;
+  }
+
+  return isRe;
+}
+
+bool GameInput::getKeyDown(int iPinInput, bool isTrueState, byte& bInputBuffRead)
+{
+  bool isRe = false;
+
+  bool isBtnUp = HIGH;
+  bool isBtnDown = LOW;
+
+  if (isTrueState == HIGH)
+  {
+    isBtnUp = LOW;
+    isBtnDown = HIGH;
+  } else {
+    isBtnUp = HIGH;
+    isBtnDown = LOW;
+  }
+
+  byte bInputRead  = digitalRead(iPinInput);
+
+  if (bInputRead == isBtnDown && bInputBuffRead == isBtnUp)
+  {
+    isRe = true;
+  }
+  bInputBuffRead = bInputRead;
+  return isRe;
+}
+
+bool GameInput::getKeyUp(int iPinInput, bool isTrueState, byte& bInputBuffRead)
+{ bool isRe = false;
+
+  bool isBtnUp = HIGH;
+  bool isBtnDown = LOW;
+
+  if (isTrueState == HIGH)
+  {
+    isBtnUp = LOW;
+    isBtnDown = HIGH;
+  } else {
+    isBtnUp = HIGH;
+    isBtnDown = LOW;
+  }
+
+  byte bInputRead  = digitalRead(iPinInput);
+
+  if (bInputRead == isBtnUp && bInputBuffRead == isBtnDown)
+  {
+    isRe = true;
+  }
+  bInputBuffRead = bInputRead;
+  return isRe;
+}
---
+++ PagonGameDev_GameLoop.h
@@ -0,0 +1,37 @@
+//================================================
+// Dev by : Pagon Game Dev
+// PagonGameDev@gmail.com
+//================================================
+
+#ifndef PagonGameDev_GameLoop_h
+#define PagonGameDev_GameLoop_h
+
+#include "Arduino.h"
+
+class GameInterval
+{
+  public:
+    GameInterval(int iInputInterval, void (funcInterval)());
+    void update();
+    float DeltaTime;
+
+
+  private:
+    int iMillisInterval;
+    int iMillisDelayTime;
+    int iMillisIntervalDelayTime;
+
+    unsigned long lMillisPrevious;
+    unsigned long lMillisCurrent;
+    void(*functionInterval)();
+};
+
+class GameInput
+{
+  public:
+    GameInput();
+    static bool getKey(int iPinInput, bool isTrueState);
+    static bool getKeyDown(int iPinInput, bool isTrueState, byte& bInputBuffRead);
+    static bool getKeyUp(int iPinInput, bool isTrueState, byte& bInputBuffRead);
+};
+#endif
